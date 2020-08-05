3f747e9bd8fd9a4a19bb31612b90fd4e727bd125
Alexander Reany <Alex.Reany@capstonelogistics.com>
2020-08-04 22:10:22 -0400

    Stamped out API structure

---
+++ KitchenHelper.Data/Database/Sql/KitchenHelperDbContext.cs
@@ -0,0 +1,15 @@
+﻿using KitchenHelper.API.Data.Entities.DbEntities;
+using Microsoft.EntityFrameworkCore;
+
+namespace KitchenHelper.API.Data.Database.Sql
+{
+    class KitchenHelperDbContext : DbContext
+    {
+        public KitchenHelperDbContext(DbContextOptions<KitchenHelperDbContext> options)
+           : base(options)
+        {
+        }
+
+        public DbSet<Recipe> Recipes { get; set; }
+    }
+}
