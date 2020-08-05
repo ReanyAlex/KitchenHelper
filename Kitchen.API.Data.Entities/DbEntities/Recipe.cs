29c43aa4c7dd0c10518365323f9b694542909cd7
Alexander Reany <Alex.Reany@capstonelogistics.com>
2020-08-04 22:02:48 -0400

    Add project files.

---
+++ Kitchen.API.Data.Entities/DbEntities/Recipe.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace KitchenHelper.API.Data.Entities.DbEntities
+{
+    public class Recipe
+    {   [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; }
+    }
+}
