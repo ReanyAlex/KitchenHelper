5fd63f0b1cace37001c432a541bbfc7ec6f1d5d1
Alexander Reany <Alex.Reany@capstonelogistics.com>
2020-08-09 15:21:39 -0400

    Removed mocked data, stoped dropping DB on start

---
+++ KitchenHelper.Data/Migrations/20200809191821_propdecorations.cs
@@ -0,0 +1,138 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace KitchenHelper.API.Data.Migrations
+{
+    public partial class propdecorations : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "RecipeIngredientInformation",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "RecipeIngredientInformation",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "RecipeIngredientInformation",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "RecipeStep",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "RecipeStep",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "RecipeStep",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Ingredients",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Ingredients",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Measurements",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Measurements",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Recipes",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Recipes",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Category",
+                table: "Recipes",
+                maxLength: 100,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Category",
+                table: "Recipes",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 100,
+                oldNullable: true);
+
+            migrationBuilder.InsertData(
+                table: "Ingredients",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Milk" },
+                    { 2, "Cookie" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Measurements",
+                columns: new[] { "Id", "Name", "ShortHand" },
+                values: new object[,]
+                {
+                    { 1, "Cup", "C" },
+                    { 2, "Each", "Each" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Recipes",
+                columns: new[] { "Id", "Category", "Description", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Test", "A nice cold glass of milk", "Cup of Milk" },
+                    { 2, "Test", "A nice cold glass of milk with cookies", "Cup of Milk With Cookies" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "RecipeIngredientInformation",
+                columns: new[] { "Id", "IngredientId", "MeasurementId", "Quantity", "RecipeId" },
+                values: new object[,]
+                {
+                    { 1, 1, 1, 2, 1 },
+                    { 2, 1, 1, 2, 2 },
+                    { 3, 2, 2, 4, 2 }
+                });
+
+            migrationBuilder.InsertData(
+                table: "RecipeStep",
+                columns: new[] { "Id", "Order", "RecipeId", "Step" },
+                values: new object[,]
+                {
+                    { 1, 1, 1, "Drink Milk" },
+                    { 2, 1, 2, "Drink Milk" },
+                    { 3, 2, 2, "Eat Cookie" }
+                });
+        }
+    }
+}
