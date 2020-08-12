15f02c5e5d3c2709a7b1c53f222edb370d1381cd
Alexander Reany <Alex.Reany@capstonelogistics.com>
2020-08-11 21:27:48 -0400

    Added User Class

--- KitchenHelper.API.Data.Tests/Dtos/RecipeDtoTests.cs
+++ KitchenHelper.API.Data.Tests/Dtos/RecipeDtoTests.cs
@@ -8,10 +8,10 @@ namespace KitchenHelper.API.Data.Tests.Dtos
     public class RecipeDtoTests
     {
         [Fact]
-        public void ClassType_RecipeDto_PropertiesCount_Returns5()
+        public void ClassType_RecipeDto_PropertiesCount_Returns6()
         {
             Type t = typeof(RecipeDto);
-            Assert.Equal(5, t.GetProperties().Count());
+            Assert.Equal(6, t.GetProperties().Count());
         }
     }
 }
---
+++ KitchenHelper.Data/Migrations/20200812011938_User.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace KitchenHelper.API.Data.Migrations
+{
+    public partial class User : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UsersRecipe",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<int>(nullable: false),
+                    RecipeId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UsersRecipe", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_UsersRecipe_Recipes_RecipeId",
+                        column: x => x.RecipeId,
+                        principalTable: "Recipes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_UsersRecipe_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UsersRecipe_RecipeId",
+                table: "UsersRecipe",
+                column: "RecipeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UsersRecipe_UserId",
+                table: "UsersRecipe",
+                column: "UserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UsersRecipe");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
