479f9b286802cad50378762b13082537e3c95c27
Alexander Reany <Alex.Reany@capstonelogistics.com>
2020-08-14 13:01:37 -0400

    Add Ids onto response body

--- Kitchen.API.Data.Entities/Dtos/IngredientDto.cs
+++ Kitchen.API.Data.Entities/Dtos/IngredientDto.cs
@@ -2,6 +2,8 @@
 {
     public class IngredientDto
     {
+        public int Id { get; set; }
+
         public string Name { get; set; }
     }
 }
--- Kitchen.API.Data.Entities/Dtos/MeasurementDto.cs
+++ Kitchen.API.Data.Entities/Dtos/MeasurementDto.cs
@@ -2,6 +2,8 @@
 {
     public class MeasurementDto
     {
+        public int Id { get; set; }
+
         public string Name { get; set; }
 
         public string ShortHand { get; set; }
